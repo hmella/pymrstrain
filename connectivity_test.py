1ff763604b9682ef2ace96a214e6aa10ec185f50
hmella <hmella@uc.cl>
2020-01-08 23:04:09 -0300

    Add connectivity image generations

--- connectivity_test.py
+++ connectivity_test.py
@@ -11,12 +11,16 @@ x = np.array([[0,0],
 # Pixels centers
 #      |0|1|2|
 #      |3|4|5|
-c = [np.array([0,1,2,0,1,2]),np.array([0,0,0,-1,-1,-1])]
+#      |6|7|8|
+c = [np.array([0,1,2,0,1,2,0,1,2]),
+     np.array([0,0,0,-1,-1,-1,-2,-2,-2])]
 width = 1.0
 shape = [2,3]
 
 # Connectivity
-p2s = [[0,1,2], [3], [4]]                     # pixel-to-spins map
+p2s = [[0,1,2],[3],[4],
+       [],[],[],
+       [],[],[]]                     # pixel-to-spins map
 s2p = -np.ones([x.shape[0],],dtype=np.int64)  # spin-to-pixel map
 n = len(p2s)
 for i in range(n):
@@ -27,7 +31,7 @@ print(s2p)
 xpx = np.array([(x[i,:]-[c[0][s2p[i]],c[1][s2p[i]]])/width for i in range(x.shape[0])])
 
 # Displacement field
-u = np.array([[0.55,-0.6],
+u = np.array([[2,-1],
               [0,0],
               [0,0],
               [0,0],
@@ -39,15 +43,21 @@ xu = x + u
 
 # Displacement in terms of pixels
 (pixel_u, subpixel_u) = np.divmod(u, width)
+(pixel_u_2, subpixel_u_2) = np.divmod(subpixel_u, 0.5*width)
 print((pixel_u,subpixel_u))
-print()
-
-# Check if spins have changed its location inside the
-(subpixel_u_, subsubpixel_u) = np.divmod(0.5*width - xpx, 0.5*width)
-print(subpixel_u_)
+print((pixel_u_2,subpixel_u_2))
 
 # Change spins connectivity according to the new positions
 for j in range(x.shape[0]):
     if s2p[j] != -1:
-        s2p[j] -= (shape[1]*pixel_u[j,1] + pixel_u[j,0])
+        s2p[j] -= shape[1]*pixel_u[j,1] + shape[1]*pixel_u_2[j,1]
+        s2p[j] += pixel_u[j,0] + pixel_u_2[j,0]
+print(p2s)
 print(s2p)
+
+# Update pixel-to-spins connectivity
+p2s = [[] for j in range(n)]
+for spin, pixel in enumerate(s2p):
+  if pixel != -1:
+    p2s[pixel].append(spin)
+print(p2s)
