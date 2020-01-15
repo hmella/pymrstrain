Fix calculation of voxel size and generation of grids

PyMRStrain/Image.py
@@ -39,7 +39,7 @@ class ImageBase(object):
 
   # Pixel size
   def voxel_size(self):
-    return self.FOV/(self.resolution-1)
+    return self.FOV/self.resolution
 
   # Practical resolution
   # OBS: resolution of the image is intended to be the number of x-coordinates
@@ -75,14 +75,17 @@ class ImageBase(object):
     # Image dimension
     d = resolution.size
 
-    # np.meshgrid generation
-    X = [np.linspace(-0.5*self.FOV[i], 0.5*self.FOV[i], resolution[i]) + self.center[i] for i in range(d)]
+    # Pixel size
+    pxsz = self.voxel_size()
 
-    if d == 3 and resolution[2] == 1:
-      X[2] = np.array(self.center[2])
-    elif d < 3:
-      X.append(np.array(self.center[2]))
-    grid = np.meshgrid(X[0], X[1], X[2], indexing='xy', sparse=sparse)
+    # np.meshgrid generation
+    X = [pxsz[i]*np.linspace(0,resolution[i]-1,resolution[i]) + self.center[i] for i in range(d)]
+    X = [X[i] - X[i].mean() for i in range(d)]
+
+    if d == 2:
+      grid = np.meshgrid(X[0], X[1], indexing='xy', sparse=sparse)
+    elif d == 3:
+      grid = np.meshgrid(X[0], X[1], X[2], indexing='xy', sparse=sparse)
 
     return grid
 

demo/2D/demo_DENSE_3D.py
@@ -6,10 +6,10 @@ import time
 if __name__=="__main__":
 
   # Parameters
-  p = Parameters_2D(decimals=10, time_steps=18)
-  np.save("p.npy", p)
+  # p = Parameters_2D(decimals=10, time_steps=18)
+  # np.save("p.npy", p)
   p=np.load('p.npy',allow_pickle=True).item()
-  p["mesh_resolution"] = 0.0015
+  p["mesh_resolution"] = 0.002
 
   # Field inhomogeneity
   phi = lambda X, Y: (X+Y)/0.1*0.2
@@ -18,13 +18,13 @@ if __name__=="__main__":
   ke = 0.12               # encoding frequency [cycles/mm]
   ke = 1000*2*np.pi*ke    # encoding frequency [rad/m]
   N = 33                  # resolution
-  I0 = DENSEImage(FOV=np.array([0.1, 0.1, 0.008]),
-            resolution=np.array([N, N, 2]),
+  I0 = DENSEImage(FOV=np.array([0.1, 0.1, 0.12]),
+            resolution=np.array([N, N, 3]),
             encoding_frequency=np.array([ke,ke,0]),
             T1=0.85,
             flip_angle=15*np.pi/180,
             off_resonance=phi,
-            kspace_factor=16)
+            kspace_factor=6)
 
   # Generator
   g0 = Generator(p, I0)
@@ -49,37 +49,12 @@ if __name__=="__main__":
   print(end-start)
 
   # Add noise to images
-  sigma = 0.4e-02
+  sigma = 0.4e-12
   un0 = add_noise_to_DENSE_(u0, mask, sigma=sigma)
   un1 = add_noise_to_DENSE_(u1, mask, sigma=sigma)
 
   # Corrected image
   u = un0 - un1
-
-  # # Plot kspace
-  # fig, ax = plt.subplots(1,2)
-  # fig0 = ax[0].imshow(abs(FFT(un0[...,0,10])),cmap=plt.get_cmap('gray'),vmin=0,vmax=3.5e+01)
-  # fig1 = ax[1].imshow(abs(FFT(u[...,0,10])),cmap=plt.get_cmap('gray'),vmin=0,vmax=7.0e+01)
-  # fig0.axes.get_xaxis().set_visible(False)
-  # fig0.axes.get_yaxis().set_visible(False)
-  # fig1.axes.get_xaxis().set_visible(False)
-  # fig1.axes.get_yaxis().set_visible(False)
-  # plt.savefig('DENSE')
-  # plt.show()
-
-  # Plot kspace
-  # fig, ax = plt.subplots(1,3)
-  # fig0 = ax[0].imshow(np.angle(np.angle(uin[...,0,10])),cmap=plt.get_cmap('gray'),vmin=-np.pi,vmax=np.pi)
-  # fig1 = ax[1].imshow(np.angle(u[...,0,10]),cmap=plt.get_cmap('gray'),vmin=-np.pi,vmax=np.pi)
-  # fig2 = ax[2].imshow(np.angle(u[...,0,10]*(-uin[...,0,10])),cmap=plt.get_cmap('gray'),vmin=-np.pi,vmax=np.pi)
-  # fig0.axes.get_xaxis().set_visible(False)
-  # fig0.axes.get_yaxis().set_visible(False)
-  # fig1.axes.get_xaxis().set_visible(False)
-  # fig1.axes.get_yaxis().set_visible(False)
-  # fig2.axes.get_xaxis().set_visible(False)
-  # fig2.axes.get_yaxis().set_visible(False)
-  # plt.savefig('DENSE')
-  # plt.show()
 
   # fig, ax = plt.subplots(1,2)
   # fig0 = ax[0].imshow(np.abs(u[...,0,0,6]),cmap=plt.get_cmap('gray'))
@@ -88,24 +63,12 @@ if __name__=="__main__":
   # fig0.axes.get_yaxis().set_visible(False)
   # fig1.axes.get_xaxis().set_visible(False)
   # fig1.axes.get_yaxis().set_visible(False)
-  # plt.savefig('moving_slice_dir')
-  # plt.show()
-
-  # fig = plt.imshow(np.angle(u[...,0,10]*np.conj(-uin[...,0,10])),cmap=plt.get_cmap('gray'),vmin=-np.pi,vmax=np.pi)
-  # fig.axes.get_xaxis().set_visible(False)
-  # fig.axes.get_yaxis().set_visible(False)
-  # plt.savefig('DENSE_B0_corrected')
-  # plt.show()
-
-  # fig = plt.imshow(np.angle(u[...,0,10]),cmap=plt.get_cmap('gray'),vmin=-np.pi,vmax=np.pi)
-  # fig.axes.get_xaxis().set_visible(False)
-  # fig.axes.get_yaxis().set_visible(False)
-  # plt.savefig('DENSE_B0_free')
+  # plt.savefig('1_slice')
   # plt.show()
 
   # Plot
   if rank==0:
     fig, ax = plt.subplots(1, 2)
-    tracker = IndexTracker(ax, np.abs(u[:,:,1,0,:]), np.angle(u[:,:,1,0,:]))
+    tracker = IndexTracker(ax, np.abs(u[:,:,0,0,:]), np.angle(u[:,:,0,0,:]))
     fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
     plt.show()