3a2535e23c4109ee9fde86a049ed802dcd285c14
hernanmella <hmella@uc.cl>
2020-01-22 17:11:46 -0300

    Moves magnetization outside of the file Generator.py

---
+++ PyMRStrain/Magnetizations.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# DENSE magnetization
+def DENSE_Mxy0(mask, M, M0, alpha, prod, t, T1, ke, X, u, phi):
+    return mask*(+0.5*M*np.exp(-t/T1)*np.exp(-1j*ke*u)
+         + 0.5*M*np.exp(-t/T1)*np.exp(-1j*ke*(2*X+u))
+         + M0*(1 - np.exp(-t/T1))*np.exp(-1j*ke*(X+u)))*prod*np.sin(alpha)*np.exp(1j*phi)
+
+# Complementary DENSE magnetization 
+def DENSE_Mxy1(mask, M, M0, alpha, prod, t, T1, ke, X, u, phi): 
+    return mask*(-0.5*M*np.exp(-t/T1)*np.exp(-1j*ke*u)
+        + -0.5*M*np.exp(-t/T1)*np.exp(-1j*ke*(2*X+u))
+        + M0*(1 - np.exp(-t/T1))*np.exp(-1j*ke*(X+u)))*prod*np.sin(alpha)*np.exp(1j*phi)
+
+# Reference DENSE magnetization
+def DENSE_Mxyin(mask, M, M0, alpha, prod, t, T1, ke, phi):
+    return mask*(M0*(1 - np.exp(-t/T1)))*prod*np.sin(alpha)*np.exp(1j*phi)
+
+# DENSE magnetizations
+def DENSEMagnetizations():
+    return DENSE_Mxy0, DENSE_Mxy1, DENSE_Mxyin
