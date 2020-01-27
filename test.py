61a62aed0f19ce3369803ed99e5ed52cc45ccf7e
hernanmella <hmella@uc.cl>
2020-01-27 19:12:01 -0300

    Add test for PyMRStrain/Imaging.py function

--- test.py
+++ test.py
@@ -1,27 +1,9 @@
 import numpy as np
+from PyMRStrain.Imaging import *
 
-# Resolution
-r = [3,3,3]
-pxsz = [1,1,1]
+# Coordinates
+z = np.linspace(-1,1,1000)
+slice_center = 0.0
+slice_thickness = 0.2
 
-# Grid
-X = np.meshgrid(np.linspace(-1,1,r[0]),np.linspace(-1,1,r[1]),np.linspace(-1,1,r[2]))
-Xf = [x.flatten('F') for x in X]
-# for x in Xf:
-#   print(x)
-
-Z = [Xf[2].min(), Xf[2].max()]
-
-# Number of additional slices
-new_slices = [2, 2]
-r_slice = r[0]*r[1]
-for i in range(len(new_slices)):
-  for j in range(new_slices[i]):
-    for k in range(r[2]):
-      if k != 2:
-        Xf[k] = np.append(Xf[k], Xf[k][0:r_slice], axis=0)
-      else:
-        Xf[k] = np.insert(Xf[k], i*Xf[k].size, Z[i]*np.ones([r_slice,]) + (j+1)*pxsz[i]*(-1)**(i+1))
-
-for x in Xf:
-  print(x)
+slice_profile(z, slice_center, slice_thickness, Nb_samples=1e+04)
