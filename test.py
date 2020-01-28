867807a17bee074d991ae6af6021f3c09f196d92
hernanmella <hmella@uc.cl>
2020-01-28 19:07:24 -0300

    test for EPI-like artifacts

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import numpy as np
-from PyMRStrain.Imaging import *
+from PyMRStrain.MRImaging import *
 
-# Coordinates
-z = np.linspace(-1,1,1000)
-slice_center = 0.0
-slice_thickness = 0.2
+# Image
+I = np.zeros([30,60])
+pxsz = [0.003, 0.003]
 
-slice_profile(z, slice_center, slice_thickness, Nb_samples=1e+04)
+# Image coordinates
+[X,Y] = np.meshgrid(np.linspace(-1,1,60),np.linspace(-1,1,30))
+R = X**2 + Y**2
+I[R <= 0.5**2] = 1
+
+acquisition_artifact(I, pxsz[1], receiver_bandwidth=32*1000, T2star=0.02)
