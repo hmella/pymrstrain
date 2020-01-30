498da1936f8badda56fb899fa44d95e3a459be1b
hmella <hmella@uc.cl>
2020-01-30 09:57:44 -0300

    Add description

--- PyMRStrain/Helpers.py
+++ PyMRStrain/Helpers.py
@@ -1,2 +1,9 @@
+# Array orderings, which depend on the emasurement
+# direction
 order = ['F','C']
+
+# Measurement and phase directions
+# The first element of m_dirs[i] (with i the encoding)
+# direction) denotes the measurement direction, and the
+# second the phase direction
 m_dirs = [[0,1], [1,0]]
