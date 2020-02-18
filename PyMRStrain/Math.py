6685ae22046a731ac60ca43410bf63d07a2f050f
hernanmella <hmella@uc.cl>
2020-02-18 11:35:51 -0300

    Add number suffixes

--- PyMRStrain/Math.py
+++ PyMRStrain/Math.py
@@ -1,6 +1,10 @@
 import numpy as np
 from numpy.fft import fftn, fftshift, ifftn, ifftshift
 
+MEGA = 1e+06
+KILO = 1e+03
+MILI = 1e-02
+MICRO = 1e-06
 
 def itok(x, axes=[0,1]):
     return fftshift(fftn(ifftshift(x,axes=axes),axes=axes),axes=axes)
