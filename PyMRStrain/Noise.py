3898bb0221d419b243f2b36529850f850d6d927d
hernanmella <hmella@uc.cl>
2020-02-28 17:09:37 -0300

    Fix noise addition without mask

--- PyMRStrain/Noise.py
+++ PyMRStrain/Noise.py
@@ -19,7 +19,7 @@ def get_segmentation_mask(u_image, tol=1.0e-15):
 
 
 # Add complex noise
-def add_cpx_noise(I, mask=[], sigma=[], SNR=20, ref=0, recover_noise=False):
+def add_cpx_noise(I, mask=1, sigma=[], SNR=20, ref=0, recover_noise=False):
 
   # Standard deviation
   if not sigma:
@@ -32,10 +32,6 @@ def add_cpx_noise(I, mask=[], sigma=[], SNR=20, ref=0, recover_noise=False):
   else:
     peak = max([np.abs(I[...,0,0]).max(), np.abs(I[...,1,0]).max()])
     sigma = sigma*peak
-
-  # Check mask argument
-  if (mask is None) or (mask is []):
-      mask = 1
 
   # Noise generation and addition
   noise = np.random.normal(0, sigma, I.shape) + 1j*np.random.normal(0, sigma, I.shape)
