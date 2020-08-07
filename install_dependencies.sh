0c039fdcc2f22da08d50577b3e9126a468bd4363
hmella <hmella@uc.cl>
2020-08-06 23:04:21 -0400

    Update dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,12 +2,19 @@
 
 # Install pymrstrain dependencies
 sudo apt-get -y install build-essential python3-dev python3-pip python3-tk \
-                 python3-setuptools libopenmpi-dev mpich gmsh cmake nano
-sudo pip3 install pybind11
-sudo pip3 install mpi4py
-sudo pip3 install meshio
-sudo pip3 install scipy
-sudo pip3 install matplotlib
+                 python3-setuptools libopenmpi-dev mpich cmake nano
+pip3 install pybind11 --user
+pip3 install mpi4py --user
+pip3 install scipy --user
+pip3 install matplotlib --user
+
+# Download and install matplotlib
+sudo apt-get install libfreetype6-dev pkg-config
+wget https://github.com/matplotlib/matplotlib/archive/v3.2.2.zip &&
+unzip v3.2.2.zip &&
+cd matplotlib-3.2.2 &&
+python3 setup.py --user &&
+cd .. && rm -rf matplotlib-3.2.2 v3.2.2.zip
 
 # Clone, build and install Eigen3-dev
 git clone https://github.com/eigenteam/eigen-git-mirror.git && \
