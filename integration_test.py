8c61b8560e3a8033f6e78d31dc5190fbde9bb684
hmella <hernan.mella@pucv.cl>
2023-07-12 16:23:37 -0400

    Added test for the integration framework

---
+++ integration_test.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Fem import assemble, massAssemble, massAssembleInt
+import meshio
+
+if __name__ == '__main__':
+
+  # Import mesh
+  mesh = meshio.read('tube.msh')
+  elems = mesh.cells_dict['tetra']
+  nodes = mesh.points 
+  print(elems)
+  print(nodes)
+
+  # Values to be assembled
+  values = np.zeros([nodes.shape[0], 3])
+  values[:] = 2.0
+
+  # Call to assemble function
+  integral = assemble(elems,nodes,values)
+  print(integral)
+
+  integral = massAssembleInt(elems,nodes,values)
+  print(integral)
