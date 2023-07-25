3c0b803220f9d3ef5222f4a16d7a0ae51be84d7d
hmella <hernan.mella@pucv.cl>
2023-07-25 18:07:57 -0400

    Updated plot test

--- 4dflow_test_plot.py
+++ 4dflow_test_plot.py
@@ -18,8 +18,10 @@ if __name__ == '__main__':
   # I = add_cpx_noise(I, relative_std=0.05, mask=1)
 
   # Show figure
-  for i in range(3):
-    multi_slice_viewer(np.abs(K[::2,:,:,i,7]))
-    multi_slice_viewer(np.abs(I[:,:,:,i,7]))
-    multi_slice_viewer(np.angle(I[:,:,:,i,7]),caxis=[-np.pi,np.pi])
-    plt.show()
+  for fr in range(K.shape[-1]):
+    print(fr)
+    for i in [0]:
+      multi_slice_viewer(np.abs(K[::2,:,:,i,fr]))
+      multi_slice_viewer(np.abs(I[:,:,:,i,fr]))
+      multi_slice_viewer(np.angle(I[:,:,:,i,fr]),caxis=[-np.pi,np.pi])
+      plt.show()
