0b233b8bd6ab47de45a953fa85ad287ed6aed297
hmella <hernan.mella@pucv.cl>
2023-07-26 17:14:09 -0400

    Added timings for debugging

--- 4dflow_test.py
+++ 4dflow_test.py
@@ -1,13 +1,16 @@
+import time
+
 import matplotlib.pyplot as plt
 import meshio
 import numpy as np
 from Fem import massAssemble
-from FlowToImage import FlowImage3Dv3
+from FlowToImage import FlowImage3D
 
 from PyMRStrain.KSpaceTraj import Cartesian
 from PyMRStrain.Math import itok, ktoi
-from PyMRStrain.MPIUtilities import gather_image, MPI_rank, MPI_comm
+from PyMRStrain.MPIUtilities import MPI_comm, MPI_rank, gather_image
 from PyMRStrain.Plotter import multi_slice_viewer
+
 
 def Rx(tx):
   return np.array([[1, 0, 0],
@@ -56,7 +59,7 @@ if __name__ == '__main__':
 
     # Kspace parameters for sampling in kz
     FOV_z    = 0.2
-    res_z    = 100
+    res_z    = 10#100
     delta_kz = 1.0/FOV_z 
     BW_kz    = 1.0/(FOV_z/res_z)
     kz       = np.arange(-0.5*BW_kz, 0.5*BW_kz, delta_kz)
@@ -67,11 +70,9 @@ if __name__ == '__main__':
 
     # Kspace trajectory
     FOV = np.array([0.1325, 0.1], dtype=np.float64)
-    res = np.array([66, 50], dtype=np.int64)
+    res = np.array([10, 10], dtype=np.int64)#np.array([66, 50], dtype=np.int64)
     traj = Cartesian(FOV=FOV, res=res, oversampling=2, lines_per_shot=5, VENC=VENC)
-    print("Echo time = {:.1f} ms".format(1000.0*traj.echo_time))
-    # if MPI_rank==0:
-    #   traj.plot_trajectory()
+    if MPI_rank==0: print("Echo time = {:.1f} ms".format(1000.0*traj.echo_time))
 
     # Assemble mass matrix for integrals (just once)
     M = massAssemble(elems,nodes)
@@ -80,6 +81,7 @@ if __name__ == '__main__':
     K = np.zeros([traj.ro_samples, traj.ph_samples, len(kz), 3, reader.num_steps], dtype=complex)
 
     # Generate images
+    times = []
     for fr in range(reader.num_steps):
 
       # Read velocity data in each time step
@@ -93,10 +95,11 @@ if __name__ == '__main__':
       velocity /= 100
 
       # Generate 4D flow image
-      print(MPI_rank, "Generating frame {:d}".format(fr))
-      # K[:,traj.local_idx,i,fr] = FlowImage3D(M, traj.points, 1000*traj.times, velocity, nodes, T2, VENC, kz[i])
-      # K[traj.local_idx,:,:,fr] = np.transpose(np.array(FlowImage3Dv2(M, traj.points, kz, traj.times, traj.vel_enc_time, velocity, nodes, T2, VENC)), axes=(1,2,0))
-      K[traj.local_idx,:,:,:,fr] = FlowImage3Dv3(M, traj.points, kz, traj.times, velocity, nodes, T2, VENC)
+      if MPI_rank == 0: print("Generating frame {:d}".format(fr))
+      t0 = time.time()
+      K[traj.local_idx,:,:,:,fr] = FlowImage3D(MPI_rank, M, traj.points, kz, traj.times, velocity, nodes, T2, VENC)
+      t1 = time.time()
+      times.append(t1-t0)
 
       # Synchronize MPI processes
       MPI_comm.Barrier()
@@ -105,19 +108,24 @@ if __name__ == '__main__':
       K_copy = np.copy(K)
       K_copy = gather_image(K_copy)
       K_copy[:,1::2,...] = K_copy[::-1,1::2,...]
-      if MPI_rank==0:
-        np.save('kspace_test',K_copy)
+      if MPI_rank==0: np.save('kspace_test',K_copy)
 
-    # # Gather results
-    # K = gather_image(K)
+      # Synchronize MPI processes
+      MPI_comm.Barrier()
 
-    # # Fix dimensions
-    # K[:,1::2,...] = K[::-1,1::2,...]
-    # I = ktoi(K[::2,::-1,...],[0,1,2])
+    # Show mean time that takes to generate each 3D volume
+    print(np.array(times).mean())
 
-    # # Export generated data
-    # if MPI_rank==0:
-    #   np.save('kspace_test',K)
+    # Gather results
+    K = gather_image(K)
+
+    # Fix dimensions
+    K[:,1::2,...] = K[::-1,1::2,...]
+    I = ktoi(K[::2,::-1,...],[0,1,2])
+
+    # Export generated data
+    if MPI_rank==0:
+      np.save('kspace_test',K)
 
     # # Show figure
     # if MPI_rank==0:
