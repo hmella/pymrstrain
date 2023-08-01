eebd11db2200d6bdd0ee2d66fc773fc7aa6cb123
hmella <hernan.mella@pucv.cl>
2023-08-01 11:47:09 -0400

    Changed double data types to float to allow faster calculations

--- PyMRStrain/src/FlowToImage.cpp
+++ PyMRStrain/src/FlowToImage.cpp
@@ -6,90 +6,31 @@
 #include <Eigen/Sparse>
 
 
+using namespace Eigen;
 namespace py = pybind11;
 
-
 // Definition of PI and complex unit
-const double PI = std::atan(1.0)*4.0;
-const std::complex<double> ii(0, 1);
+const float PI = std::atan(1.0)*4.0;
+const std::complex<float> ii(0, 1);
 
 // Definition of magnetization datatype for 4d flow expression
-typedef std::tuple<Eigen::MatrixXcd,
-  Eigen::MatrixXcd> magnetizations;
+typedef std::tuple<MatrixXcf,
+  MatrixXcf> magnetizations;
 
 // Definition of third rank complex tensor datatype
-typedef Eigen::Tensor<std::complex<double>, 4> ComplexTensor;
-
-
-// 4D flow magnetizations (FourierExp can be added here)
-Eigen::MatrixXcd FlowMags(
-  const Eigen::Ref<const Eigen::MatrixXd> &v,
-  const double &VENC){
-
-    // Thermanl equilibrium magnetization
-    const double M0 = 1000.0*v.rows();
-
-    // Real and imaginary magnetizations
-    Eigen::MatrixXcd Mxy = M0*((ii*(PI/VENC)*v).array().exp());
-
-  return Mxy;
-}
-
-
-// Fourier exponential
-void FourierExp(Eigen::VectorXcd& Exp,
-  const Eigen::Ref<const Eigen::MatrixXd> &r,
-  const double &kx,
-  const double &ky,
-  const double &kz){
-
-    // Generate magnetizations
-    // Obs: the 2*pi term is not in the exponential because is included
-    // in (kx,ky,kz), which were multiplied in the function FlowImage
-    Exp.noalias() = (-ii*(r.col(0)*kx + r.col(1)*ky + r.col(2)*kz)).array().exp().matrix();
-
-}
-
-
-// Fourier exponential
-void zFourierExp(
-  Eigen::VectorXcd& Exp,
-  const Eigen::Ref<const Eigen::VectorXcd>& ExpXY,
-  const Eigen::Ref<const Eigen::MatrixXd> &r,
-  const double &kz){
-
-    // Generate magnetizations
-    // Obs: the 2*pi term is not in the exponential because is included
-    // in (kx,ky,kz), which were multiplied in the function FlowImage
-    Exp(Eigen::all,0) = (ExpXY - ii*r.col(2)*kz).array().exp();
-
-}
-
-
-// Fourier exponential
-void xyFourierExp(Eigen::VectorXcd& Exp,
-  const Eigen::Ref<const Eigen::MatrixXd> &r,
-  const double &kx,
-  const double &ky){
-
-    // Generate magnetizations
-    // Obs: the 2*pi term is not in the exponential because is included
-    // in (kx,ky,kz), which were multiplied in the function FlowImage
-    Exp.noalias() = -ii*(r.col(0)*kx + r.col(1)*ky);
-
-}
+typedef Tensor<std::complex<float>, 4> ComplexTensor;
 
 // Calculate image kspace
 ComplexTensor FlowImage3D(
   const int &MPI_rank,
-  const Eigen::SparseMatrix<double> &M, // Mass matrix
-  const std::vector<Eigen::MatrixXd> &kxy, // kspace trajectory
-  const Eigen::VectorXd &kzz,
-  const Eigen::MatrixXd &t,  // kspace timings
-  const Eigen::MatrixXd &v,  // object velocity
-  const Eigen::MatrixXd &r0, // object position
-  const double &T2,          // T2 time of the blood
-  const double &VENC){       // Velocity encoding
+  const SparseMatrix<float> &M, // Mass matrix
+  const std::vector<MatrixXf> &kxy, // kspace trajectory
+  const VectorXf &kzz,
+  const MatrixXf &t,  // kspace timings
+  const MatrixXf &v,  // object velocity
+  const MatrixXf &r0, // object position
+  const float &T2,          // T2 time of the blood
+  const float &VENC){       // Velocity encoding
 
     // Number of kspace lines/spokes/interleaves
     const uint nb_lines = kxy[0].cols();
@@ -105,61 +46,52 @@ ComplexTensor FlowImage3D(
 
     // Get the equivalent gradient needed to go from the center of the kspace
     // to each location
-    const Eigen::MatrixXd kx = kxy[0]*(2.0*PI);
-    const Eigen::MatrixXd ky = kxy[1]*(2.0*PI);
-    const Eigen::VectorXd kz = kzz*(2.0*PI);
+    const MatrixXcf kx = kxy[0]*(2.0*PI);
+    const MatrixXcf ky = kxy[1]*(2.0*PI);
+    const VectorXcf kz = kzz*(2.0*PI);
 
     // Copy blood position to estimate the current position using the approximation r(t0+dt) = r0 + v0*dt
-    Eigen::MatrixXd r = r0;
+    MatrixXcf r = r0;
 
-    // Build magnetizations and Fourier exponential
-    Eigen::MatrixXcd Mag = FlowMags(v, VENC);
-    Eigen::VectorXcd fe = Eigen::MatrixXcd::Zero(nb_spins, 1);
-    Eigen::VectorXcd fexy = Eigen::MatrixXcd::Zero(nb_spins, 1);
-    // Eigen::VectorXcd fez = Eigen::MatrixXcd::Zero(nb_spins, 1);
+    // Kspace and Fourier exponential
+    MatrixXcf Mxy = 1000.0*nb_spins*((ii*(PI/VENC)*v).array().exp());
+    VectorXcf fe = MatrixXcf::Zero(nb_spins, 1);
 
     // Image kspace
-    ComplexTensor Mxy(nb_meas, nb_lines, nb_kz, 3);
+    ComplexTensor Ks(nb_meas, nb_lines, nb_kz, 3);
 
     // T2* decay
-    double T2_decay = 0.0;
+    const MatrixXcf T2_decay = (-t/T2).array().exp();
 
     // Iterate over kspace measurements/kspace points
-    for (uint j = 0; j < nb_lines; j++){
+    for (uint j = 0; j < nb_lines; ++j){
 
       // Debugging
-      if (MPI_rank == 0){
-        py::print("  ky location ", j);
-      }
+      if (MPI_rank == 0){ py::print("  ky location ", j); }
 
       // Iterate over slice kspace measurements
-      for (uint i = 0; i < nb_meas; i++){
+      for (uint i = 0; i < nb_meas; ++i){
 
         // Update blood position at time t(i,j)
         r.noalias() = r0 + v*t(i,j);
 
-        // T2* decay
-        T2_decay = std::exp(-t(i,j)/T2);
-
         // Fourier exponential
-        xyFourierExp(fexy, r, kx(i,j), ky(i,j));
+        fe.noalias() = -(r.col(0)*kx(i,j) + r.col(1)*ky(i,j));
 
-        for (uint k = 0; k < nb_kz; k++){
+        for (uint k = 0; k < nb_kz; ++k){
 
           // Update Fourier exponential
-          // FourierExp(fe, r, kx(i,j), ky(i,j), kz(k));
-          zFourierExp(fe, fexy, r, kz(k));
-          // fe.noalias() = fexy.cwiseProduct(fez);
+          fe = ii*(fe - r.col(2)*kz(k)).array().exp();
 
           // Calculate k-space values, add T2* decay, and assign value to output array
           for (uint l = 0; l < 3; l++){
-            Mxy(i,j,k,l) = (M*Mag.col(l).cwiseProduct(fe)).sum()*T2_decay;
+            Ks(i,j,k,l) = (M*Mxy.col(l).cwiseProduct(fe)).sum()*T2_decay(i,j);
           }
         }
       }
     }
 
-    return Mxy;
+    return Ks;
 }
 
 
